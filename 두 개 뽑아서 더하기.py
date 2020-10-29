570b735179544b7a1762cd81ec0f57735420b7d8
repl.it user <kmj990929@dgist.ac.kr>
2020-10-29 15:06:06 +0000

    Solve 두 개 뽑아서 더하기

--- 두 개 뽑아서 더하기.py
+++ 두 개 뽑아서 더하기.py
@@ -1,3 +1,11 @@
+from itertools import permutations
+
 def solution(numbers):
-  answer = []
-  return answer
+    answer = []
+    permute = permutations(numbers,2)
+    for a,b in permute:
+        answer.append(a+b)
+    answer = list(set(answer))
+    answer.sort()
+
+    return answer
